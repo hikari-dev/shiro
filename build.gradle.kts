c9fa8a63ad0e0a74fe4a3b528a150e5c42c3a8e1
hikari <zhiyoushangdicainengshenpanwo@gmail.com>
2021-01-16 14:22:18 +0800

    Bump gradle version to 6.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ tasks.withType<Jar> {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("net.mamoe:mirai-core:2.0-RC")
+    implementation("net.mamoe:mirai-core:2.0.0")
 
     implementation("com.charleskorn.kaml:kaml:0.26.0")
 
