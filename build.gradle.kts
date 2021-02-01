fb5dfa40f740a2480caa5fbdf03798aa20d68d29
hikari <zhiyoushangdicainengshenpanwo@gmail.com>
2021-02-01 09:37:16 +0800

    Update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ tasks.withType<Jar> {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("net.mamoe:mirai-core:2.1.1")
+    implementation("net.mamoe:mirai-core:2.2.2")
 
     implementation("com.charleskorn.kaml:kaml:0.26.0")
 
