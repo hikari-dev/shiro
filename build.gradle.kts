3cf4a39408ecb544d4129976c4a1774e1372d930
hikari <zhiyoushangdicainengshenpanwo@gmail.com>
2021-02-25 10:56:31 +0800

    Update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,6 @@ dependencies {
     implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
     implementation("mysql:mysql-connector-java:8.0.22")
-    implementation("com.zaxxer", "HikariCP", "3.4.5")
+    implementation("com.zaxxer", "HikariCP", "4.0.2")
 
 }
