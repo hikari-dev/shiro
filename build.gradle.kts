17cc9bab4fc2892ed32d8de4dda9004815f8052c
hikari <zhiyoushangdicainengshenpanwo@gmail.com>
2021-03-02 10:24:32 +0800

    Update mirai to 2.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ tasks.withType<Jar> {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("net.mamoe", "mirai-core", "2.4.0")
+    implementation("net.mamoe", "mirai-core", "2.4.1")
 
     implementation("com.charleskorn.kaml:kaml:0.26.0")
 
