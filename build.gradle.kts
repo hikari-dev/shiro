18868180ec70a5debe1a2643ba008745c25c7b11
hikari <zhiyoushangdicainengshenpanwo@gmail.com>
2021-06-25 09:52:18 +0800

    Don't use bom!!!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 plugins {
-    val kotlinVersion = "1.5.10"
+    val kotlinVersion = "1.5.20"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
     id("com.github.johnrengelman.shadow") version "7.0.0"
@@ -46,10 +46,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
     implementation("org.quartz-scheduler:quartz:2.3.2")
 
-    implementation(platform("org.jetbrains.exposed:exposed-bom:0.32.1"))
-    implementation("org.jetbrains.exposed", "exposed-core")
-    implementation("org.jetbrains.exposed", "exposed-dao")
-    implementation("org.jetbrains.exposed", "exposed-jdbc")
+    val exposedVersion = "0.32.1"
+    implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
     implementation("mysql:mysql-connector-java:8.0.25")
     implementation("com.zaxxer", "HikariCP", "4.0.2")
 
