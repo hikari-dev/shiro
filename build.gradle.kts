9d8d1e6dbf67bbd23e3a896148553783149aac3c
hikari <zhiyoushangdicainengshenpanwo@gmail.com>
2021-08-28 01:04:52 +0800

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 plugins {
-    val kotlinVersion = "1.5.20"
+    val kotlinVersion = "1.5.30"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
     id("com.github.johnrengelman.shadow") version "7.0.0"
@@ -39,7 +39,7 @@ tasks.withType<Test> {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("net.mamoe", "mirai-core", "2.6.7")
+    implementation("net.mamoe", "mirai-core", "2.7.0")
 
     implementation("com.charleskorn.kaml:kaml:0.35.2")
 
