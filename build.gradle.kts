74e5097edf474d7f96ad3eca8edebfcd4f799238
hikari-dev <zhiyoushangdicainengshenpanwo@gmail.com>
2022-07-04 10:08:42 +0800

    chore: bump mirai version

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ tasks.withType<Test> {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("net.mamoe", "mirai-core", "2.11.1")
+    implementation("net.mamoe", "mirai-core", "2.12.0")
 
     implementation("com.charleskorn.kaml:kaml:0.46.0")
 
